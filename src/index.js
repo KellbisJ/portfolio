92877ed1b8a9a8b501c489ed9a72970807687869
Kellbis <holasoykellbis@gmail.com>
2025-02-13 13:01:42 -0400

    better layout, using font awesome, projects section made, copy email made

--- src/index.js
+++ src/index.js
@@ -1,13 +1,75 @@
 (() => {
+	// navbar logic
 	const menuButton = document.querySelector('#menu');
-	const NAVBAR_MENU_CONTENT = document.querySelector('.navbar__menu__content');
-	const MENU_ICON = document.querySelector('#menu-icon');
+	const navbarMenuContent = document.querySelector('.navbar__menu__content');
+	const menuIcon = document.querySelector('#menu-icon');
 
-	menuButton.addEventListener('click', () => {
-		const menuIcons = ['menu', 'close'];
+	let menuNavbarShowed = false;
 
-		NAVBAR_MENU_CONTENT.classList.toggle('show');
+	const menuIcons = ['fa fa-bars', 'fa-solid fa-x'];
 
-		NAVBAR_MENU_CONTENT.classList.contains('show') ? (MENU_ICON.innerHTML = menuIcons[1]) : (MENU_ICON.innerHTML = menuIcons[0]);
-	});
+	const toggleMenu = () => {
+		navbarMenuContent.classList.toggle('show');
+
+		navbarMenuContent.classList.contains('show')
+			? ((menuIcon.classList = menuIcons[1]), (menuNavbarShowed = true))
+			: ((menuIcon.classList = menuIcons[0]), (menuNavbarShowed = false));
+	};
+
+	const handleBodyClick = (event) => {
+		if (menuNavbarShowed && !navbarMenuContent.contains(event.target) && !menuButton.contains(event.target)) {
+			navbarMenuContent.classList.remove('show');
+			menuNavbarShowed = false;
+			menuIcon.classList = menuIcons[0];
+		}
+	};
+
+	menuButton.addEventListener('click', toggleMenu);
+	document.body.addEventListener('click', handleBodyClick);
+
+	navbarMenuContent.addEventListener('click', (event) => {
+		event.stopPropagation();
+	}); // navbar logic
+
+	// copy email logic
+
+	const emailAddress = 'kellbisdevsw@gmail.com';
+
+	const copyElement = document.getElementById('copyElement');
+	const mailIcon = document.getElementById('mailIcon');
+	const emailInput = document.getElementById('emailInput');
+	const copyText = document.getElementById('copyText');
+
+	const copyAddres = async () => {
+		try {
+			await navigator.clipboard.writeText(emailInput.value);
+			copyElement.classList.add('copy-success');
+			copyText.classList.remove('hidden');
+			setTimeout(() => {
+				copyElement.classList.remove('copy-success');
+				copyText.classList.add('hidden');
+			}, 1700);
+		} catch (err) {
+			console.error(`err copying text: ${err}`);
+			fallbackCopyAddress();
+		}
+	};
+
+	const fallbackCopyAddress = () => {
+		emailInput.select();
+		document.execCommand('copy');
+		copyElement.classList.add('copy-success');
+		copyText.classList.remove('hidden');
+		setTimeout(() => {
+			copyElement.classList.remove('copy-success');
+			copyText.classList.add('hidden');
+		}, 1700);
+	};
+
+	const openMailTo = () => {
+		window.location.href = `mailto:${emailAddress}`;
+	};
+
+	copyElement.addEventListener('click', copyAddres);
+	mailIcon.addEventListener('click', openMailTo); // copy email logic
 })();
