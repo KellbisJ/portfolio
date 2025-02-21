b0de8a4ecd1200169640ef1c7278ae1ea9e2c609
Kellbis <holasoykellbis@gmail.com>
2025-02-21 09:35:19 -0400

    certificate modal begiginnings

--- src/components/my-studies/index.js
+++ src/components/my-studies/index.js
@@ -1,4 +1,4 @@
-() => {
+(() => {
   document.addEventListener("DOMContentLoaded", () => {
     const jsStudiesReferences = [
       "https://platzi.com/home/",
@@ -18,5 +18,42 @@
         chronology: "2019 - Actualidad [2025]",
       },
     };
+
+    const certificateModal = document.querySelector(".modal");
+    const showCertificateBtn = document.querySelector(".show-certificate-btn");
+
+    const toggleShowCertificate = () => {
+      certificateModal.classList.toggle("show");
+
+      if (certificateModal.classList.contains("show")) {
+        insertCertificateDocument();
+      }
+    };
+
+    const insertCertificateDocument = () => {
+      const modalContent = document.createElement("div");
+      const spanClose = document.createElement("span");
+      const closeIcon = document.createElement("i");
+      const documentIframe = document.createElement("iframe");
+
+      modalContent.classList.add("modal-content");
+      spanClose.classList.add("close");
+      closeIcon.classList.add("fa-solid", "fa-circle-xmark");
+      documentIframe.src = "https://drive.google.com/file/d/1E28ok_rk0Xx4Hoc8wExadZ-97qT0SVXk/preview";
+      documentIframe.allow = "autoplay";
+
+      spanClose.appendChild(closeIcon);
+      spanClose.addEventListener("click", () => {
+        certificateModal.classList.remove("show");
+        certificateModal.innerHTML = "";
+      });
+
+      modalContent.appendChild(spanClose);
+      modalContent.appendChild(documentIframe);
+
+      return certificateModal.appendChild(modalContent);
+    };
+
+    showCertificateBtn.addEventListener("click", toggleShowCertificate);
   });
-};
+})();
