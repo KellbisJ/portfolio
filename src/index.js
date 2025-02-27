ec451b043438c7d569b4d720a4b4de1a88350101
Kellbis <holasoykellbis@gmail.com>
2025-02-27 11:43:50 -0400

    relative paths?

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 (() => {
   document.addEventListener("DOMContentLoaded", () => {
     const moduleScripts = [
-      "/src/language/index.js",
-      "/src/components/navbar/index.js",
-      "/src/components/preview-me/index.js",
-      "/src/components/projects/index.js",
-      "/src/components/technologies/index.js",
-      "/src/components/my-studies/index.js",
-      "/src/components/about-me/index.js",
-      "/src/components/contact-me/index.js",
-      "/src/language/eventEmitter.js",
-      "/src/scripts/particles-js/index.js",
+      "src/language/index.js",
+      "src/components/navbar/index.js",
+      "src/components/preview-me/index.js",
+      "src/components/projects/index.js",
+      "src/components/technologies/index.js",
+      "src/components/my-studies/index.js",
+      "src/components/about-me/index.js",
+      "src/components/contact-me/index.js",
+      "src/language/eventEmitter.js",
+      "src/scripts/particles-js/index.js",
     ];
 
     const cdnScripts = ["https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"];
